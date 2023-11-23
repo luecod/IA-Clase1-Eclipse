f0402ffa771d46324149732c366590da323adb5e
Luis Valladolid <eduvalladolid1997@gmail.com>
2023-11-23 15:35:09 -0500

    primer commit

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module primero {
+	requires jgap;
+}
---
+++ src/primero/za.java
@@ -0,0 +1,10 @@
+package primero;
+
+public class za {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
